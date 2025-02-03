05bc5b3a5acb698ec13a97fd7bb07f385feb41a4
Deltaur <jessy.belair018@gmail.com>
2025-02-02 20:11:34 -0500

    added count_words and report functions to gather basic info about book and then create a basic report about it

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("hello world")
+def open_book(book):
+    with open(book) as f:
+        file_contents = f.read()    
+    return file_contents
+
+def count_words(text):
+    words = text.lower()
+    chars = {}
+    for word in words:
+        for char in word:
+            if char in chars: 
+                chars[char] += 1
+            else:
+                chars[char] = 1
+    return chars
+    
+def report(data):
+    for letter in data:
+        if letter.isalpha() == True: print(f"The '{letter}' character was found {data[letter]} times")
+    print("--- End report ---")
+
+def main():
+    text = open_book("books/frankenstein.txt")
+    data = count_words(text)
+    print(report(data))
+    
+main()
